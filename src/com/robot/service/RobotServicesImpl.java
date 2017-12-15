b0fd7554723c247d1cf1c0991dfe3e2ec4eae900
Diego Benavides <diegobenavides@MacBook-Air-de-Diego.local>
2017-12-14 22:43:37 -0500

    [CHANGE] New version of service with Maven format. Was tested with local robot.

--- src/com/robot/service/RobotServicesImpl.java
+++ src/com/robot/service/RobotServicesImpl.java
@@ -1,5 +1,8 @@
 package com.robot.service;
 
+import java.io.PrintStream;
+import java.net.Socket;
+
 import com.robot.beans.Robot;
 
 public class RobotServicesImpl implements RobotServices {
@@ -12,15 +15,42 @@ public class RobotServicesImpl implements RobotServices {
 		robot.setY(y);
 		robot.setZ(z);
 		
-		try {
-			System.out.println("Coordinates:" + robot.toString());
+		try
+        {
+            /* Se crea el socket cliente */
+            //Socket socket = new Socket ("localhost", 25557);
+			Socket socket = new Socket ("localhost", 55000);
+            System.out.println ("conectado");
 
-			return true;
-		} catch(Exception e) {
-			System.err.println("Err: " + e);
-
-			return false;
-		}
+            PrintStream ps = new PrintStream(socket.getOutputStream());
+            
+            String s = "y";
+            ps.write(s.getBytes("UTF-8"));
+            ps.flush();
+            Thread.sleep(1000);
+            ps.write(String.valueOf(x).getBytes("UTF-8"));
+            ps.flush();
+            Thread.sleep(1000);
+            ps.write(String.valueOf(y).getBytes("UTF-8"));
+            ps.flush();
+            Thread.sleep(1000);
+            ps.write(String.valueOf(z).getBytes("UTF-8"));
+            ps.flush();
+            ps.write(s.getBytes("UTF-8"));
+            ps.flush();
+            
+            Thread.sleep(1000000);
+            
+            ps.close();
+            socket.close();
+            
+            return true;
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return false;
+        }
 	}
 
 }
